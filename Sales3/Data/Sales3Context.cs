87b470fc2bc45f02888fd36560e2f7e41233b6bb
Raphael L. da Costa <rcosta1977@outlook.com>
2021-05-18 11:06:31 +0100

    CRUD Scaffolding

---
+++ Sales3/Data/Sales3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sales3.Models;
+
+namespace Sales3.Data
+{
+    public class Sales3Context : DbContext
+    {
+        public Sales3Context (DbContextOptions<Sales3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sales3.Models.Department> Department { get; set; }
+    }
+}
